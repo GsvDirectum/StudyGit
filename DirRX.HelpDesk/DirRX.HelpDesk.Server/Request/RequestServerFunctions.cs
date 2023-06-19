7a3e3f75869aad8d4059b0be4d40ef10547da0e7
gituser <gituser@test.test>
2023-06-19 16:35:01 +0400

    Create Request and add one Property Number

---
+++ DirRX.HelpDesk/DirRX.HelpDesk.Server/Request/RequestServerFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using DirRX.HelpDesk.Request;
+
+namespace DirRX.HelpDesk.Server
+{
+  partial class RequestFunctions
+  {
+
+    /// <summary>
+    /// 
+    /// </summary>       
+    public void Function()
+    {
+      var a=2;
+    }
+
+  }
+}
